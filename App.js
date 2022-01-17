7ea016334a15c365a48320252f6224900ac132a2
Daniel Ethridge <danielethridge@pop-os.localdomain>
2022-01-17 17:59:30 -0500

    preliminary work

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import "./style.css"
 
 const App = () => {
     return (
-        <h1>Daniel Ethridge</h1>
+        <div>
+            <h1 className='name-title'>Daniel Mark Ethridge</h1>
+            <h2 className='job-title'>Software Engineer at the Georgia Tech Research Insttitute</h2>
+            <p1>
+                My passions lie at the intersection of software development and music.
+                I currently am employed at the Georgia Tech Research Institute (GTRI) 
+                where I am a Research Scientist I and Software Developer.
+            </p1>
+        </div>
     )
 }
 
