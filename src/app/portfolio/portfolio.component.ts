aa436c633f67a453eef4df5f4bf6f00cc95dee9f
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2024-03-23 16:33:58 -0600

    Added a new service, created functionality for the featured project

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,26 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
 import { Tag } from '../_models/Tag';
+import { ProjectsService } from '../_services/projects.service';
+
+/* 
+TODO 
+  - What is a service?
+  - ngOnInit
+*/
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
-export class PortfolioComponent {
 
-  project: Project = {
-    id: 0,
-    name: "Sample angular app",
-    summary: "test description", 
-    description: "",
-    projectLink: "",
-    tags: [Tag.ANGULAR],
-    pictures: []
+export class PortfolioComponent implements OnInit {
+
+  projects = {} as Project[];
+
+  constructor(private titleService: Title, private projectService: ProjectsService) {
+    this.titleService.setTitle("Daniel Ethridge - Portfolio");
   }
 
-  constructor(private titleService: Title) {
-    this.titleService.setTitle("Daniel Ethridge - Portfolio");
+  ngOnInit(): void {
+    this.projects = this.projectService.getProjects();
   }
 }
