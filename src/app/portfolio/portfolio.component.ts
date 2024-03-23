b17706bfb5155949ae16331a2e5128b7be7d08ce
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2024-03-23 16:16:01 -0600

    Updated project cards to use newly created Project.ts and Tag.ts code in order to streamline the process of creating project cards

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from '../_models/Project';
+import { Tag } from '../_models/Tag';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,6 +9,17 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
+
+  project: Project = {
+    id: 0,
+    name: "Sample angular app",
+    summary: "test description", 
+    description: "",
+    projectLink: "",
+    tags: [Tag.ANGULAR],
+    pictures: []
+  }
+
   constructor(private titleService: Title) {
     this.titleService.setTitle("Daniel Ethridge - Portfolio");
   }
