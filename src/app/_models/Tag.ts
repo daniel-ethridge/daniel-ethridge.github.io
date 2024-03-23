d0e4cf7e63bf40f9da250cfe5dda6729202ac28b
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2024-03-23 16:16:01 -0600

    Updated project cards to use newly created Project.ts and Tag.ts code in order to streamline the process of creating project cards

---
+++ src/app/_models/Tag.ts
@@ -0,0 +1,18 @@
+/*
+TODO:
+    - Look up keywords: static, readonly, difference between them
+    - Look up interface vs class in javascript/typescript
+    - Look up public and private
+*/
+
+export class Tag {
+    static readonly ANGULAR = new Tag("Angular", "red");
+    
+    private constructor(private readonly key: string, public readonly color: string) {
+
+    }
+
+    toString() {
+        return this.key;
+    }
+}
