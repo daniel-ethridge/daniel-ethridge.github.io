067927fc6b5879ec6c2bd05a36f2d354b399ab9f
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2024-03-24 18:27:33 -0600

    Added photo assets back in. Experimenting with different things

--- src/app/_models/Tag.ts
+++ src/app/_models/Tag.ts
@@ -7,7 +7,11 @@ TODO:
 
 export class Tag {
     static readonly ANGULAR = new Tag("Angular", "red");
-    static readonly TYPESCRIPT = new Tag("Angular", "red");
+    static readonly TYPESCRIPT = new Tag("Angular", "darkred");
+    static readonly CSHARP = new Tag("C#", "green");
+    static readonly UNITY = new Tag("Unity", "lightgreen");
+    static readonly WWISE = new Tag("WWise", "darkgreen");
+    static readonly COMPOSITION = new Tag("Music Composition", "lightblue");
     
     private constructor(private readonly key: string, public readonly color: string) {
 
