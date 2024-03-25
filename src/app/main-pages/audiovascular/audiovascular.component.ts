6e107170c14f9af00dae14fb1b337cf0f102c1a9
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2024-03-25 00:56:39 -0600

    Rearranged files, added audiovascular page, did some basic design exploration

---
+++ src/app/main-pages/audiovascular/audiovascular.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audiovascular',
+  templateUrl: './audiovascular.component.html',
+  styleUrls: ['./audiovascular.component.css']
+})
+export class AudiovascularComponent {
+
+}
