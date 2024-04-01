2d59e65fe6404d506c375e783ff6248d1ee136f4
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2024-04-01 12:16:27 -0400

    Added a simple contact page

--- src/app/main-pages/contact/contact.component.ts
+++ src/app/main-pages/contact/contact.component.ts
@@ -4,8 +4,9 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css', '../../app.component.css']
 })
+
 export class ContactComponent {
   constructor(private titleService: Title) {
     this.titleService.setTitle("Daniel Ethridge - Contact");
