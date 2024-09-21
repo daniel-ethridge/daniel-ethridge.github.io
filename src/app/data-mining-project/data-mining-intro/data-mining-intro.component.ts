051444a3807311b600be8525f4a199342e8fc7ff
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2024-09-21 21:04:43 +0200

    Website backbone for data mining project set up

---
+++ src/app/data-mining-project/data-mining-intro/data-mining-intro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-mining-intro',
+  templateUrl: './data-mining-intro.component.html',
+  styleUrls: ['./data-mining-intro.component.css']
+})
+export class DataMiningIntroComponent {
+
+}
---
+++ src/app/data-mining-project/data-mining-navigation/data-mining-navigation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-mining-navigation',
+  templateUrl: './data-mining-navigation.component.html',
+  styleUrls: ['./data-mining-navigation.component.css']
+})
+export class DataMiningNavigationComponent {
+
+}
