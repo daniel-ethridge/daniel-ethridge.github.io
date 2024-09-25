c0825fc52f75d2eb8324201276ce669495e2688b
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2024-09-25 14:37:46 +0200

    Team page completed. Introduction page almost done. Data mining project

--- src/app/_models/ClassRoutes.ts
+++ src/app/_models/ClassRoutes.ts
@@ -1,4 +1,5 @@
 export class ClassRoutes {
+    // Machine Learning
     mlIntroduction: string = "portfolio/machine-learning/introduction";
     mlEda: string = "portfolio/machine-learning/data-prep-and-eda";
 }
--- src/app/data-mining-project/data-mining-team/data-mining-team.component.ts
+++ src/app/data-mining-project/data-mining-team/data-mining-team.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-mining-team.component.css']
 })
 export class DataMiningTeamComponent {
+  atharvaPhoto: string = "../../../assets/data-mining-assets/team/atharva-photo.jpg";
+  atharvaLinkedin: string = "http://www.linkedin.com/in/atharva-patil-714b77222";
+  atharvaGithub: string = "https://github.com/Atharva309";
 
+  danielPhoto: string = "../../../assets/profile-photo.jpeg";
+  danielLinkedin: string = "https://www.linkedin.com/in/daniel-ethridge1/";
+  danielGithub: string = "https://github.com/daniel-ethridge";
+
+  deepPhoto: string = "../../../assets/data-mining-assets/team/deep-photo.jpg";
+  deepGithub: string = "https://github.com/623dks";
+  deepLinkedin: string = "https://www.linkedin.com/in/deep-shukla-b4035220a/";
+
+  sujithPhoto: string = "../../../assets/data-mining-assets/team/sujith-photo.jpeg";
+  sujithGithub: string = "https://github.com/larry6683";
+  sujithLinkedin: string = "https://www.linkedin.com/in/battu-sujith/";
+
+  linkedinIcon: string = "../../../assets/linkedin-logo.png";
+  githubIcon: string = "../../../assets/github-logo.jpeg";
 }
