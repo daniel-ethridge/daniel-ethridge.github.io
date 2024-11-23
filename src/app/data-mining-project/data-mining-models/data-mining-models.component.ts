51d9f98d2919061636277e5d7ad76d7b5f488931
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2024-11-22 20:52:23 -0700

    Added information for association rule mining

--- src/app/data-mining-project/data-mining-models/data-mining-models.component.ts
+++ src/app/data-mining-project/data-mining-models/data-mining-models.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class DataMiningModelsComponent {
 
+  armPlots: string[] = [
+    "../../../assets/data-mining-assets/visualizations/arm/liked-action.png",
+    "../../../assets/data-mining-assets/visualizations/arm/liked-adventure.png",
+    "../../../assets/data-mining-assets/visualizations/arm/liked-casual.png",
+    "../../../assets/data-mining-assets/visualizations/arm/liked-indie.png",
+    "../../../assets/data-mining-assets/visualizations/arm/liked-multiplayer.png",
+    "../../../assets/data-mining-assets/visualizations/arm/liked-racing.png",
+    "../../../assets/data-mining-assets/visualizations/arm/liked-rpg.png",
+    "../../../assets/data-mining-assets/visualizations/arm/liked-simulation.png",
+    "../../../assets/data-mining-assets/visualizations/arm/liked-story.png",
+    "../../../assets/data-mining-assets/visualizations/arm/liked-strategy.png",
+  ]
 }
