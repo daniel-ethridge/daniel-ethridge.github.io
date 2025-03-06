68c11a75f73e8f7ae535af69c45aeb89ec8dd3a3
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2025-03-06 16:35:12 -0700

    Added new style elements, updated machine learning project

--- src/app/ml-project/ml-page-navigation/ml-page-navigation.component.ts
+++ src/app/ml-project/ml-page-navigation/ml-page-navigation.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ml-page-navigation',
   templateUrl: './ml-page-navigation.component.html',
-  styleUrls: ['./ml-page-navigation.component.css']
+  styleUrls: [
+    './ml-page-navigation.component.css',
+    '../../styles/material-overrides.css'
+  ]
 })
 
 export class MlPageNavigationComponent {
