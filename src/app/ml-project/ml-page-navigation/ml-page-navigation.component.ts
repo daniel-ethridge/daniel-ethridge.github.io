a0f2b4724c667c53fc4a05d30a51306742dd65ad
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2025-03-06 19:39:40 -0700

    All projects updated. All pages now using new design

--- src/app/ml-project/ml-page-navigation/ml-page-navigation.component.ts
+++ src/app/ml-project/ml-page-navigation/ml-page-navigation.component.ts
@@ -4,9 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-ml-page-navigation',
   templateUrl: './ml-page-navigation.component.html',
   styleUrls: [
-    './ml-page-navigation.component.css',
-    '../../styles/material-overrides.css'
-  ]
+    './ml-page-navigation.component.css']
 })
 
 export class MlPageNavigationComponent {
