a589b6a98e91876938e807c98b674767497359dd
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2025-04-03 13:53:18 -0600

    Updated information for soup data

--- src/app/project-pages/soup/soup.component.ts
+++ src/app/project-pages/soup/soup.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-soup',
   templateUrl: './soup.component.html',
-  styleUrls: ['./soup.component.css']
+  styleUrls: ['./soup.component.css'],
+  standalone: false
 })
 export class SoupComponent {
 
