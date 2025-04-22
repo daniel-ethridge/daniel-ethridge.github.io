6b265898a9172064403dd8a475132313a890d48c
Daniel Ethridge <ethridge.daniel.m@gmail.com>
2025-04-22 13:27:54 -0600

    Working on text mining

--- src/app/project-pages/text-mining-project/text-mining-models/text-mining-nb/text-mining-nb.component.ts
+++ src/app/project-pages/text-mining-project/text-mining-models/text-mining-nb/text-mining-nb.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { MathScript } from 'src/app/_models/MathScript';
+import { MathRenderer, MathService } from 'src/app/_services/math.service';
 
 @Component({
     selector: 'app-text-mining-nb',
@@ -6,6 +8,11 @@ import { Component } from '@angular/core';
     styleUrls: ['./text-mining-nb.component.scss'],
     standalone: false
 })
-export class TextMiningNbComponent {
+export class TextMiningNbComponent implements AfterViewInit{
 
+      math: MathScript = new MathScript();
+    
+      ngAfterViewInit() {
+          new MathRenderer(new MathService).render();
+        }
 }
